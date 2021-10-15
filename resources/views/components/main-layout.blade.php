b7164c23a9cab9ac550824a411353744083e397a
mohammedobaid1234 <mhmd.obaid.18@gmail.com>
2021-10-15 11:22:08 +0300

    15/10

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -214,6 +214,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <p>قسم الجرائد الالكترونية</p>
                 </a>
               </li>
+              <li class="nav-item">
+                <a href="{{route('ads.index')}}" class="nav-link @if (Route::is('ads.*'))
+                {{'active'}}
+              @endif">
+                  <i class="fas fa-file-pdf"></i>
+                  <p>قسم الاعلانات</p>
+                </a>
+              </li>
             </ul>
           </li>
           <li class="nav-item">
